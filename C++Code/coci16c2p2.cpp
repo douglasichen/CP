398ffa7f4277b72ea5c3c365c8f678784100bc25
dch23 <dcvweb123@gmail.com>
2022-05-20 16:55:57 -0400

    e

--- C++Code/coci16c2p2.cpp
+++ C++Code/coci16c2p2.cpp
@@ -13,7 +13,28 @@ int main() {
 	cin.tie(0);
 	
 	ll N,M,K,X; cin>>N>>M>>K>>X;
-	vector<ll> b;
 	
+	ll at=X-1;
+	vector<ll> loc(M);
+	for (int i=0; i<M; i++) {
+		loc[M-i-1]=at%K;
+		at/=K;
+	}
+
+	string S; cin>>S;
+	vector<char> place(M);
+	for (int i=0; i<M; i++) {
+		string s; cin>>s; sort(s.begin(),s.end());
+		place[i]=s[loc[i]];
+	}
+	int e=0;
+	for (int i=0; i<N; i++) {
+		if (S[i]=='#') {
+			S[i]=place[e];
+			e++;
+		}
+	}
+	cout << S << endl;
+
 
 }
