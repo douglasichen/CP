1b53105b245155d9ae8e687daac732d0489e53a2
Douglas <dcvweb123@gmail.com>
2022-10-04 23:29:58 -0400

    e

---
+++ Grouped/dmoj/acc1p3.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    ll N,T,md=998244353; cin>>N>>T;
+    vector<ll> A(N), B(N);
+    for (int i=0; i<N; i++) cin>>A[i]>>B[i];
+    ll L=1, R=1e18;
+    while (L<R) {
+        ll M=L+R>>1, sum=0, took=0;
+        for (int i=0; i<N; i++) {
+            ll a=A[i], b=B[i], r=a%b,
+                k=(a-r-((M-r)/b+1)*b)/b+1,
+                K=(a-r)/b+1, p=K-k, 
+                k1=(k%2==0 ? k/2 : k), k2=((k-1)%2==0 ? (k-1)/2 : k-1),
+                sm=(k1%md)*(k2%md)%md + (p%md)*(b%md)%md*(k%md)%md + (r%md)*(k%md)%md;
+            sm%=md;
+            sum+=sm;
+            sum%=md;
+            took+=k;
+            if (took>T) break;
+        }
+        if (took>T) L=M+1;
+        if (took<T) {
+            ll need=T-took;
+            sum+=(need%md)*(M%md)%md;
+            sum%=md;
+            for (int i=0; i<N; i++) {
+              66  ll a=A[i], b=B[i], r=a%b;
+                if (M<=a && (M-r)%b==0) need--;
+            }
+            if (need<=0) took=T;
+            if (need) R=M-1;
+        }
+        if (took==T) {
+            cout << sum << endl;
+            return 0;
+        }
+    }
+}
--- Grouped/dmoj/coi06p1.cpp
+++ Grouped/dmoj/coi06p1.cpp
@@ -3,28 +3,23 @@ using namespace std;
 
 #define endl '\n'
 
-void solve() {
-
-}
-
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
-
-	unsigned int N; cin>>N;
-	vector<unsigned int> V(N+1);
-	for (unsigned int i=1; i<=N; i++) cin>>V[i];
-	multiset<vector<unsigned int>> S;
-	long long ans=N*1LL*(N-1)/2LL;
-	for (unsigned int i=N; i>0; i--) {
-		if (i<N) {
-			auto it=S.upper_bound(vector<unsigned int>{V[i],0});
-			if (it!=S.end()) {
-				ans-=N-(*it)[1];
-			}
-		}
-		S.emplace(vector<unsigned int>{V[i],i});
-		if (i==0) break;
+	
+	long long ans=0;
+	int N; cin>>N;
+	vector<int> S;
+	map<int,int> F;
+	for (int i=0, cnt, e; i<N; i++) {
+		cin>>e;
+		cnt=0;
+		while (!S.empty() && S.top()<e) F[S.top()]--, S.pop(), cnt++;
+		int add=cnt+F[e];
+		if (add<S.size()+cnt) add++;
+		ans+=add;
+		F[e]++;
+		S.push(e);
 	}
 	cout << ans << endl;
 }
