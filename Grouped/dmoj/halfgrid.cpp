833f9ae1df5029b53bf3aa4af0432a815f4ff20f
Douglas <dcvweb123@gmail.com>
2022-11-23 21:10:42 -0500

    e

--- Grouped/dmoj/halfgrid.cpp
+++ Grouped/dmoj/halfgrid.cpp
@@ -1,50 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define endl '\n'
-#define ull unsigned long long
 
-ull md=1e9+7;
-// ull fpw(ull a, ull b) {
-// 	ull ans=1;
-// 	for (ull p=a; b; b>>=1, p*=p, p%=md) {
-// 		if (b&1) ans*=p, ans%=md;
-// 	}
-// 	return ans;
-// }
+int md=1e9+7;
+int mx=2e6;
+vector<int> spf(mx+1);
+
+long long mult,res;
+int fpow(int a, int b) {
+	mult=a, res=1;
+	while (b) {
+		if (b&1) res*=mult, res%=md;
+		mult*=mult, mult%=md;
+		b>>=1;
+	}
+	return res;
+}
+
+long long ways(int W, int H) {
+	if (W>H) swap(W,H);
+	int Z=W+H;
+	vector<int> F(Z+1);
+	for (int i=H+1,at; i<=Z; i++) {
+		at=i;
+		while (at) {
+			if (spf[at]!=0) F[spf[at]]++, at/=spf[at];
+			else F[at]++, at=0;
+		}
+	}
+	for (int i=2, at; i<=W; i++) {
+		at=i;
+		while (at) {
+			if (spf[at]!=0) F[spf[at]]--, at/=spf[at];
+			else F[at]--, at=0;
+		}
+	}
+	
+	long long ans=1;
+	for (int i=2; i<=Z; i++)
+		ans*=fpow(i,F[i]), ans%=md;
+	return ans;
+}
 
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
 
-	int W,H,X; cin>>W>>H>>X; W--, H--;
-	for (int i=0,a,b; i<X; i++) cin>>a>>b;
-	if (W==0 || H==0) {
-		cout << 1 << endl;
-		return 0;
-	}
-
-	vector<bool> isP(2e6,1);
-	
-	vector<long long> fW(2e6), fH(2e6), fWH(2e6);
-	for (int i=2; i<2e6; i++) {
-		if (!isP[i]) continue;
-		for (int at=i, cnt=1; at<2e6; at+=i, cnt++) {
-			if (at!=i) isP[at]=0;
-			if (at<=W) fW[i]+=cnt;
-			if (at<=H) fH[i]+=cnt;
-			if (at<=H+W) fWH[i]+=cnt;
+	for (int i=2,at; i<=mx; i++) {
+		at=i;
+		while (at+i<=mx) {
+			at+=i;
+			if (!spf[at]) spf[at]=i;
 		}
 	}
-
 	
-	for (int i=2; i<2e6; i++) fWH[i]-=fW[i]+fH[i];
-
-	ull ans=1;
-	for (int a=2; a<2e6; a++) {
-		for (int i=0; i<fWH[a]; i++) ans*=a, ans%=md;
+	int W,H,X,Z; cin>>W>>H>>X;
+	long long ans=ways(W-1, H-1);
+	int a,b,c,d;
+	if (X==1) {
+		cin>>a>>b;
+		ans-=ways(a-1,b-1)*ways(W-a, H-b)%md;
 	}
-	cout << ans << endl;
+	else if (X==2) {
+		cin>>a>>b>>c>>d;
+		if (a>c) swap(a,c), swap(b,d);
+		ans-=ways(a-1,b-1)*ways(W-a, H-b)%md,
+		ans-=ways(b-1,d-1)*ways(W-b, H-d)%md;
+		if (b>=d) 
+			ans-=ways(a-1,b-1)
+			*ways(c-a, d-b)%md
+			*ways(W-c, H-d)%md;
+	}
+	ans%=md;
+	while (ans<0) ans+=md;
 	
-
+	cout << ans << endl;
 }
--- Grouped/dmoj/tempCodeRunnerFile.cpp
+++ Grouped/dmoj/tempCodeRunnerFile.cpp
@@ -2,22 +2,71 @@
 using namespace std;
 #define endl '\n'
 
-int main() {
-    cin.sync_with_stdio(0);
-    cin.tie(0);
+int md=1e9+7;
+int mx=2e6;
+vector<int> spf(mx+1);
 
-    int N, K; cin>>N>>K;
-    long long ans=0;
-    vector<long long> A(N), B(N), C(N);
-    iota(C.begin(), C.end(), 0);
-    for (int i=0; i<N; i++) {
-        cin>>A[i]>>B[i];
-        B[i]-=A[i];
-        ans+=A[i];
-    }
-    auto cmp=[&](int &a, int &b) {return B[a]<B[b];};
-    sort(C.begin(), C.end(), cmp);
-    for (int i=0; i<K; i++)
-        ans+=B[C[i]];
-    cout << ans << endl;
+long long mult,res;
+int fpow(int a, int b) {
+	mult=a, res=1;
+	while (b) {
+		if (b&1) res*=mult, res%=md;
+		mult*=mult, mult%=md;
+		b>>=1;
+	}
+	return res;
+}
+
+long long ways(int W, int H) {
+	if (W>H) swap(W,H);
+	int Z=W+H;
+	vector<int> F(Z+1);
+	for (int i=H+1,at; i<=Z; i++) {
+		at=i;
+		while (at) {
+			if (spf[at]!=0) F[spf[at]]++, at/=spf[at];
+			else F[at]++, at=0;
+		}
+	}
+	for (int i=2, at; i<=W; i++) {
+		at=i;
+		while (at) {
+			if (spf[at]!=0) F[spf[at]]--, at/=spf[at];
+			else F[at]--, at=0;
+		}
+	}
+	
+	long long ans=1;
+	for (int i=2; i<=Z; i++)
+		ans*=fpow(i,F[i]), ans%=md;
+	return ans;
+}
+
+int main() {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+
+	for (int i=2,at; i<=mx; i++) {
+		at=i;
+		while (at+i<=mx) {
+			at+=i;
+			if (!spf[at]) spf[at]=i;
+		}
+	}
+
+	int W,H,X,Z; cin>>W>>H>>X;
+	long long ans=ways(W-1, H-1);
+
+	int a,b,c,d; cin>>a>>b>>c>>d;
+	if (a==b && c==d) {
+		ans-=ways(a-1,b-1)*ways(W-a, H-b)%md;
+	}
+	else {
+		ans-=ways(a-1,b-1)*ways(W-a, H-b)%md;
+		ans-=ways(c-1,d-1)*ways(W-c, H-d)%md;
+	}
+	while (ans<0) ans+=md;
+	
+	
+	cout << ans << endl;
 }
