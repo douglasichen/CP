7596e840c3a0201f7388ba309be9e080e910156e
dch23 <dcvweb123@gmail.com>
2022-11-23 21:09:18 -0500

    new stuff

--- Grouped/codeforces/1582/D.cpp
+++ Grouped/codeforces/1582/D.cpp
@@ -3,10 +3,6 @@ using namespace std;
 
 #define endl '\n'
 
-void solve() {
-
-}
-
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
@@ -14,19 +10,16 @@ int main() {
 	int T; cin>>T;
 	while (T--) {
 		int N; cin>>N;
-		vector<int> A(N), B(N), C(N);
-		for (int i=0; i<N; i++) cin>>A[i], B[i]=(A[i]<0 ? -1 : 1);
-		for (int &i : A) i=abs(i);
-		
-		iota(C.begin(), C.end(), 0);
-		sort(C.begin(), C.end(), [&](int &a, int &b) {
-			return A[a]>A[b];
-		});
-		
-		int sm=accumulate(A.begin(), A.end(), 0);
-		for (int &i : C) {
-			int b=(sm-A[i])/A[i];
-			if (b>0) B[i]*=-b, sm-=A[i]+A[i]*abs(B[i]);
+		vector<int> A(N), B(N);
+		for (int &i : A) cin>>i;
+		if (N&1) {
+			if (A[0]+A[1]) B[0]=-A[2], B[1]=-A[2], B[2]=(A[0]+A[1]);
+			else if (A[0]+A[2]) B[0]=-A[1], B[1]=(A[0]+A[2]), B[2]=-A[1];
+			else B[0]=(A[1]+A[2]), B[1]=-A[0], B[2]=-A[0];
+		}
+		for (int i=(N&1)*3; i<N; i+=2) {
+			B[i]=A[i+1];
+			B[i+1]=-A[i];
 		}
 		for (int &i : B) cout << i << ' '; cout << endl;
 	}	
