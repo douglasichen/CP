8d04ea5ee472233c0f7637ff1b723e74ab5e1c43
dch23 <dcvweb123@gmail.com>
2022-12-12 23:30:25 -0500

    usaco folders

--- Grouped/dmoj/noip00p3.cpp
+++ Grouped/dmoj/noip00p3.cpp
@@ -3,54 +3,54 @@ using namespace std;
 
 #define endl '\n'
 
-int N;
-vector<string> V;
-char init;
+void solve() {
 
-int ans=0;
-void solve(vector<int> done, string S) {
-	// cout << S << endl;
-	ans=max(ans, (int)S.size());
-	
-	
-	for (int at=0; at<N; at++) {
-		if (S.size()) {
-			if (done[at]==2) continue;
-
-			map<string,bool> pre;
-			string s="";
-			for (int i=0; i<V[at].size()-1; i++)
-				s+=V[at][i],
-				pre[s]=1;
-			for (int o=S.size()-1; o>0; o--) {
-				string mid=S.substr(o, S.size()-o);
-				if (mid.size()>=V[at].size()) break;
-				if (pre[mid]) {
-					string nS=S+V[at].substr(mid.size(), V[at].size()-mid.size());
-					vector<int> nDone=done;
-					nDone[at]++;
-					solve(nDone, nS);
-					break;
-				}
-			}
-		}
-		else {
-			if (V[at][0]!=init) continue;
-			vector<int> nDone=done;
-			nDone[at]=1;
-			solve(nDone, V[at]);
-		}
-	}
 }
 
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
 
-	cin>>N;
-	V=vector<string>(N);
-	for (int i=0; i<N; i++) cin>>V[i]; cin>>init;
-	vector<int> done(8);
-	solve(done, "");
+	int N; cin>>N;
+	vector<string> words;
+	for (int i=0; i<N; i++) {
+		string s; cin>>s;
+		words.push_back(s);
+		words.push_back(s);
+	}
+	char sC; cin>>sC;
+
+	// create graph
+	vector<vector<vector<int>>> G(N<<1); // cost, id
+	for (int a=0; a<N*2; a++) {
+		for (int b=0; b<N*2; b++) {
+			if (a!=b) {
+				for (int sz=1; sz<min(words[a].size(), words[b].size()); sz++) {
+					if (words[a].substr(words[a].size()-sz, sz)==words[b].substr(0,sz)) {
+						G[a].push_back({words[b].size()-sz,b});
+						break;
+					}
+				}
+			}
+		}
+	}
+	int ans=0;
+	priority_queue<vector<int>, vector<vector<int>>> pq;
+	for (int start=0; start<N*2; start++) {
+		if (words[start][0]==sC) {
+			vector<int> vis(N*2);
+			pq.push({words[start].size(), start});
+			while (!pq.empty()) {
+				vector<int> top=pq.top(); pq.pop();
+				vis[top[1]]=1;
+				ans=max(ans,top[0]);
+				for (vector<int> child : G[top[1]]) {
+					if (!vis[child[1]]) {
+						pq.push({top[0]+child[0], child[1]});
+					}
+				}
+			}
+		}
+	}
 	cout << ans << endl;
 }
