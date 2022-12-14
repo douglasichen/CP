a9b2fa0c8f340cb974b2a3eac7665086d036df93
Douglas <dcvweb123@gmail.com>
2022-12-14 08:20:03 -0500

    s2s

---
+++ Grouped/dmoj/ccc01s2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<vector<int>> dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+    vector<vector<int>> V(20, vector<int>(20));
+    vector<int> pos={10,10};
+    int x,y; cin>>x>>y;
+    
+    
+}
